4fe8f00472fe2561d5ab24281ea9404b744671ee
trexy94@live.it <1FvuTroa>
2021-01-29 23:34:02 +0100

    added first line parsing

--- practice_problem/kotlin/test.kts
+++ practice_problem/kotlin/test.kts
@@ -1,16 +1,34 @@
 import java.io.File
 
 //  kotlinc -script test.kts ./a_example.in
-
 args.forEach {
     println("input file -> $it")
     doStuff(it)
 }
 
-fun readFileLineByLineUsingForEachLine(fileName: String) 
-  = File(fileName).forEachLine { println(it) }
+var pizzaTotal : Int = 0
+var team2ppl: Int = 0
+var team3ppl: Int = 0
+var team4ppl: Int = 0
 
-fun doStuff(fileName: String) {
-    readFileLineByLineUsingForEachLine(fileName);
-    // todo
+fun readFileLineByLineUsingForEachLine(fileName: String) {
+    var index: Int = 0
+    File(fileName).forEachLine {
+        if (index == 0) {
+            // first line parsing
+            val (npizzaTotal, nteam2ppl, nteam3ppl, nteam4ppl) = it.split(' ')
+            pizzaTotal = npizzaTotal.toInt()
+            team2ppl = nteam2ppl.toInt()
+            team3ppl = nteam3ppl.toInt()
+            team4ppl = nteam4ppl.toInt()
+        }
+        index++
+        println(it)
+    }
 }
+
+    fun doStuff(fileName: String) {
+        readFileLineByLineUsingForEachLine(fileName)
+        // todo
+
+    }

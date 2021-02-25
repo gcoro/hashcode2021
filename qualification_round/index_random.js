ea0d017fae298b1cea39c0269741ef818b6c3a56
a-bragagnolo95 <alessia_958@hotmail.it>
2021-02-25 19:22:22 +0000

    First attempt of solution

--- qualification_round/index_random.js
+++ qualification_round/index_random.js
@@ -18,24 +18,84 @@ const getResult = (contentToParse) => {
 	const rows = contentToParse.split('\n');
 	let streets = [];
 	let cars = [];
-
-	const [simulationDuration, intersections, streetsNumber, carsNumber] = rows[0].split(' ').map(el => parseInt(el));
+	const [simulationDuration, intersectionsNumber, streetsNumber, carsNumber] = rows[0].split(' ').map(el => parseInt(el));
+	let intersectionMap = new Map();
 
 	let i;
 	for(i =1; i<=streetsNumber; i++){
-        streets.push(rows[i]);
+		let data = rows[i].split(' ');
+        streets.push({
+			startIntersection: parseInt(data[0]),
+			endIntersection: parseInt(data[1]),
+			name: data[2],
+			length: parseInt(data[3])
+        });
+
+        if(!intersectionMap.has(streets[streets.length-1].endIntersection)){
+			intersectionMap.set(streets[streets.length-1].endIntersection, {
+				value: 1,
+				streets: [streets[streets.length-1].name]
+			});
+		} else {
+			// eslint-disable-next-line no-mixed-spaces-and-tabs
+        	let intersectionEntry = intersectionMap.get(streets[streets.length-1].endIntersection);
+			// eslint-disable-next-line no-mixed-spaces-and-tabs
+        	intersectionMap.set(streets[streets.length-1].endIntersection, {
+				value: intersectionEntry.value + 1,
+				streets: [... intersectionEntry.streets, streets[streets.length-1].name]
+			});
+		}
 	}
 
 	for(i; i<=streetsNumber + carsNumber; i++){
-		cars.push(rows[i]);
+		let streets = rows[i].split(' ').slice(1);
+		cars.push({
+			id: i-1-streetsNumber,
+			streets
+		});
 	}
 
-	console.log(cars)
+	//let intersectionsScheduled = 0;
+	//let scheduledStreets = [];
+	// let scheduledStreets = [{
+	// 	intersectionId : 0,
+	// 	numberOfStreets: 0,
+	// 	streets: [['streetName', 'secondsOfGreen']]
+	// }];
 
+
+	const [intersectionsScheduled, scheduledStreets] = execute(simulationDuration, intersectionMap);
+
+
+	result.push(intersectionsScheduled);
+	for(let j =0; j< scheduledStreets.length; j++){
+		result.push(scheduledStreets[j].intersectionId);
+		result.push(scheduledStreets[j].numberOfStreets);
+		for(let k=0; k< scheduledStreets[j].streets.length; k++){
+			result.push(scheduledStreets[j].streets[k].join(' '));
+		}
+	}
 
 	const end = now();
 	console.log(`process result took ${(end - start).toFixed(3)} ms`);
     writeToFile(result);
+};
+
+
+const execute = (simulationDuration, intersectionMap) => {
+	let intersectionsScheduled = 0;
+	let scheduledStreets = [];
+
+	for (let [key, entry] of intersectionMap) {
+		let streets = entry.streets.map(el => [el, Math.floor(simulationDuration/entry.value)]);
+		scheduledStreets.push({
+			'intersectionId': key,
+			'numberOfStreets': entry.value,
+			streets
+		});
+	}
+
+	return [intersectionsScheduled, scheduledStreets];
 };
 
 const content = readContent();
